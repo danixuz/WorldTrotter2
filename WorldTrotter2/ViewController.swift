dbf38b95389c5fbadbaf315961b14476b3311790
danixuz <danixuz@icloud.com>
2022-08-11 21:43:09 +0300

    1.1

--- WorldTrotter2/ViewController.swift
+++ WorldTrotter2/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Create the gradient layer
+    let gradientLayer: CAGradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do additional stuff after the view loaded.
+        
+        // Choosing gradient colors
+        gradientLayer.colors = [
+            UIColor.purple.cgColor,
+            UIColor.blue.cgColor,
+            UIColor.green.cgColor,
+            UIColor.yellow.cgColor,
+            UIColor.red.cgColor
+        ]
+        // Rotating the gradient layer 180 degrees ( pi / 4 radians).
+        //        gradientLayer.transform = CATransform3DMakeRotation(CGFloat.pi / 4, 0, 0, 1)
+        
+        // NOW WE ADD IT AS SUB LAYER
+        view.layer.insertSublayer(gradientLayer, at: 0) // This will insrt it as sub layer (sub-view) in the safe area. This is where we want it.
+        //        view.layer.insertSublayer(gradientLayer, at: 1) // This will insert it as a sub layer and will hide the "212"
+        //        view.layer.insertSublayer(gradientLayer, at: 2) // This will insert it as a sub layer and will hide the "212" and the "degrees Fahrenheit".
+        
+        //        view.layer.insertSublayer(gradientLayer, above: view.layer) // Will put it above the entire layer ( all of the subviews ).
+        
+        
+        
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        // To position the layer we set the layer's frame to be the same as the bounds of the view controller's view.
+        let mainViewBounds = view.bounds
+        gradientLayer.frame = mainViewBounds
+        
+        // Because layer do not participate in Auto Layout in the same way that views do, we want to update the layer's frame in the view controller's viewWillLayoundSubviews() method.
+    }
+    
 }
 
